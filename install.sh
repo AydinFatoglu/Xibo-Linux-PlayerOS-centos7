d133464840091526b2ae6c9d894cb8f86922a298
AydinFatoglu <86136084+AydinFatoglu@users.noreply.github.com>
2021-06-20 00:41:17 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 clear
 xibouser="USER INPUT"
 read -p "YOU MUST ENTER A NEW USERNAME FOR THIS SIGNAGE DISPLAY: " xibouser
-id -u $xibouser &>/dev/null || useradd -m $xibouser
+id -u $xibouser &>/dev/null || useradd $xibouser
 echo "YOU MUST ENTER A NEW PASSWORD (twice) FOR USER [$xibouser]"
 passwd $xibouser
 
