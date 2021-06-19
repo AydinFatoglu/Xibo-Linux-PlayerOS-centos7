c60a47a79572c1346026c6a26ec97e2ef8360c0e
AydinFatoglu <86136084+AydinFatoglu@users.noreply.github.com>
2021-06-20 01:58:56 +0300

    Create resume.sh

---
+++ resume.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# filename: reload_bash_shell.sh
+
+# check if the reboot flag file exists. 
+# We created this file before rebooting.
+if [ ! -f /var/run/resume-after-reboot ]; then
+  echo "running script for the first time.."
+  
+  # run your scripts here
+
+  # Preparation for reboot
+  script="bash /reload_bash_shell.sh"
+  
+  # add this script to zsh so it gets triggered immediately after reboot
+  # change it to .bashrc if using bash shell
+  echo "$script" >> ~/.zshrc 
+  
+  # create a flag file to check if we are resuming from reboot.
+  sudo touch /var/run/resume-after-reboot
+  
+  echo "rebooting.."
+  # reboot here
+  
+else 
+  echo "resuming script after reboot.."
+  
+  # Remove the line that we added in zshrc
+  sed -i '/bash/d' ~/.zshrc 
+  
+  # remove the temporary file that we created to check for reboot
+  sudo rm -f /var/run/resume-after-reboot
+
+  # continue with rest of the script
+fi
