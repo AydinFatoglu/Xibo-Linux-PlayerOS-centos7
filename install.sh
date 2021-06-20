5d54bcf43471e72350ef6ba5c55b445eb1fe91fa
AydinFatoglu <86136084+AydinFatoglu@users.noreply.github.com>
2021-06-21 00:33:03 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -49,7 +49,7 @@ sudo yum install snapd -y > /dev/null 2>&1
 sudo systemctl enable --now snapd.socket > /dev/null 2>&1
 sudo ln -s /var/lib/snapd/snap /snap > /dev/null 2>&1
 
-echo "Downloading and Installing Latest Stable Version Xibo Player From Snap"
+echo "Downloading and Installing Xibo Player From Snap"
 
 sudo snap install xibo-player > /dev/null 2>&1
 sleep 5
