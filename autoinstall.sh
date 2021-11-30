adbcea589a0b8adfda8e8b99749c00d794e3e8f3
AydinFatoglu <86136084+AydinFatoglu@users.noreply.github.com>
2021-11-30 20:18:34 +0300

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -159,16 +159,13 @@ WantedBy=default.target
 EOT
 
 chmod +x /etc/systemd/system/xiboplayer.service
+systemctl enable xiboplayer.service > /dev/null 2>&1
 
 echo "Configureing  Conky"
 
 rm -f /etc/conky/conky.conf
 wget -P /etc/conky/ https://raw.githubusercontent.com/AydinFatoglu/Xibo-Linux-PlayerOS-centos7/main/conky.conf > /dev/null 2>&1
 chmod +x /etc/conky/conky.conf
-
-
-
-systemctl enable xiboplayer.service > /dev/null 2>&1
 
 echo "GUI enabled"
 systemctl set-default graphical.target > /dev/null 2>&1
