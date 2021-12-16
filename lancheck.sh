94ff0e577b062fd0312ea162be1a2cc97c236c60
AydinFatoglu <86136084+AydinFatoglu@users.noreply.github.com>
2021-12-17 00:59:02 +0300

    Update lancheck.sh

--- lancheck.sh
+++ lancheck.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while true ; do
 
-if /sbin/ethtool eth0 | grep -q "Link detected: yes"; then
+if /sbin/ethtool enp4s0 | grep -q "Link detected: yes"; then
     sleep 90
 else
     /sbin/shutdown -r now
