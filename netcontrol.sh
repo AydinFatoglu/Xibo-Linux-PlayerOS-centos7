0c99dd14e72fba071d6264b2689a35157f0123ad
AydinFatoglu <86136084+AydinFatoglu@users.noreply.github.com>
2021-12-17 20:49:35 +0300

    Update netcontrol.sh

--- netcontrol.sh
+++ netcontrol.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-while true ; do
-target="google.com"
-sleep 90
-count=$( ping -c 1 $target | grep icmp* | wc -l )
-if [ $count -eq 0 ]
-then
-
-/sbin/shutdown -r now
-else
-
+# Recommend syntax for setting an infinite while loop
+while :
+do
+sleep 60
+ping -c4 8.8.8.8
+let a=$?
+if [ "$a" != "0" ]; then
+  /sbin/shutdown -r now
 fi
+
 done
